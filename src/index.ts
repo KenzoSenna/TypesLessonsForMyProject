20f23dff3cdb2ff70184130261964f30041c1f14
KenzoSenna <kenzitos@gmail.com>
2025-09-25 16:24:24 -0400

    First commit, vê ai pedrosa

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+function showInfo(date: Date){
+    console.log(date.toLocaleDateString())
+    console.log(`Dia: ${date.getDate()} | Mês: ${date.getMonth()+1} | Ano: ${date.getFullYear()}`)
+}
+
+showInfo(new Date("2025-10-12"))
