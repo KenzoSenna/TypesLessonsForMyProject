4bc3b67227e05a882ac1c11177b4948e2aa02822
KenzoSenna <kenzoandblade@gmail.com>
2025-10-02 16:47:03 -0400

    Início da construção do código de maneira limpa e modularizada

--- build/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function showInfo(date) {
-    console.log(date.toLocaleDateString());
-    console.log(`Dia: ${date.getDate()} | Mês: ${date.getMonth() + 1} | Ano: ${date.getFullYear()}`);
-}
-showInfo(new Date("2025-10-12"));
-// testando updates do TS pro JS
-console.log("Dattebayooooo!");
