d71dfd4cdfed185d60ad43385fcf7dd07cdeca7a
KenzoSenna <kenzoandblade@gmail.com>
2025-10-02 16:37:00 -0400

    Corrigidos erros de terminal novamente, além de lógica da concatenação.

--- build/admin_to_user_system.js
+++ build/admin_to_user_system.js
@@ -28,84 +28,99 @@ function adminAction(admin, desicao) {
         admin.kick(userToBan);
     }
 }
-
-function show_menu_actions(){
-        console.log(`
-    Olá usuário!
-    O que você deseja realizar em nosso sistema?
-    -------------------
-    1 - Criar usuário
-    2 - Promover usuário
-    3 - Selecionar usuário
-    4 - Sair
-    `);
+function show_menu_actions() {
+    console.log(`
+Olá usuário!
+O que você deseja realizar em nosso sistema?
+-------------------
+1 - Criar usuário
+2 - Promover usuário
+3 - Selecionar usuário
+4 - Sair
+`);
 }
-
 function menu() {
-    show_menu_actions()    
+    show_menu_actions();
+    console.log("O que deseja fazer?");
     const guia = prompter()(": ");
     const decisao_1 = parseInt(guia);
-    if (decisao_1 === 1) {
-        const name_to_be_created = prompter()("Nome do usuário a ser criado: ");
-        const newUser = createUser(name_to_be_created);
-        const alreadyExists = users.some(user => user.name === name_to_be_created);
-        if (alreadyExists) {
-            console.log(`Usuário ${name_to_be_created} já está registrado!`);
-            menu();
-        }
-        else {
-            users.push(newUser);
-            console.log(`Usuário ${newUser.name} criado com sucesso!`);
-            menu();
-        }
-    }
-    if (decisao_1 === 2) {
-        let users_concatenada = "";
-        for (let i = 1; i < users.length; i++) {
-            users_concatenada += `|\t${users[i].name}`;
-        }
-        console.log(`Lista de usuários registrados = ${users_concatenada}`);
-        const choiced_user_name = prompter()("Digite qual usuário você deseja pegar: ");
-        const foundUser = users.find(user => user.name === choiced_user_name);
-        if (foundUser) {
-            const promoted_adminn = promoteUser(foundUser);
-            admins.push(promoted_adminn);
-            console.log(`Usuário ${foundUser.name} promovido a admin!`);
-            menu();
-        }
-        else {
-            console.log(`Usuário ${choiced_user_name} não encontrado.`);
-            menu();
-        }
-    }
-    if (decisao_1 === 3) {
-        console.log("Qual usuário você deseja ser?")
-        const found_user_name = prompter()(": ");
-        const user_to_be = users.find(user => user.name === found_user_name);
-        if (!user_to_be) {
-            console.log(`Usuário ${found_user_name} não encontrado.`);
-            menu();
+    switch (decisao_1) {
+        case (1):
+            const name_to_be_created = prompter()("Nome do usuário a ser criado: ");
+            const newUser = createUser(name_to_be_created);
+            const alreadyExists = users.some(user => user.name === name_to_be_created);
+            if (alreadyExists) {
+                console.log(`Usuário ${name_to_be_created} já está registrado!`);
+                menu();
+            }
+            else {
+                users.push(newUser);
+                console.log(`Usuário ${newUser.name} criado com sucesso!`);
+                menu();
+            }
+            break;
+        case (2):
+            // var users_concatenada: string = ""
+            // for (let i = 0; i < users.length; i++) {
+            //     users_concatenada += `|\t${users[i].name}\t`
+            // }
+            // console.log(`Lista de usuários registrados = ${users_concatenada}`)
+            console.log("Usuários registrados:");
+            users.forEach(user => {
+                console.log(`- ${user.name}`);
+            });
+            console.log("Administradores registrados:");
+            admins.forEach(admin => {
+                console.log(`- ${admin.name}`);
+            });
+            const choiced_user_name = prompter()("Digite qual usuário você deseja pegar: ");
+            const foundUser = users.find(user => user.name === choiced_user_name);
+            if (foundUser) {
+                const promoted_adminn = promoteUser(foundUser);
+                admins.push(promoted_adminn);
+                console.log(`Usuário ${foundUser.name} promovido a admin!`);
+                menu();
+            }
+            else {
+                console.log(`Usuário ${choiced_user_name} não encontrado.`);
+                menu();
+            }
+            break;
+        case (3):
+            // let admins_concatenada: string = ""
+            // for (let i = 0; i < admins.length; i++) {
+            //     admins_concatenada += `|\t${admins[i].name}\t`
+            // }
+            console.log("Usuários registrados:");
+            users.forEach(user => {
+                console.log(`- ${user.name}`);
+            });
+            console.log("Administradores registrados:");
+            admins.forEach(admin => {
+                console.log(`- ${admin.name}`);
+            });
+            const found_user_name = prompter()("Qual usuário você deseja ser? ");
+            const user_to_be = users.find(user => user.name === found_user_name);
+            if (!user_to_be) {
+                console.log(`Usuário ${found_user_name} não encontrado.`);
+                menu();
+                return;
+            }
+            const isAdmin = admins.find(admin => admin.name === user_to_be.name);
+            if (isAdmin) {
+                console.log(`Olá, Admin ${isAdmin.name}!`);
+                const acao = parseInt(prompter()("Digite 1 para banir ou 2 para kickar: "));
+                adminAction(isAdmin, acao);
+                menu();
+            }
+            else {
+                console.log(`Olá, ${user_to_be.name}! Você não possui permissões de admin.`);
+                menu();
+            }
+            break;
+        case (4):
+            console.log("Saindo...");
             return;
-        }
-        const isAdmin = admins.find(admin => admin.name === user_to_be.name);
-        if (isAdmin) {
-            console.log(`Olá, Admin ${isAdmin.name}!`);
-            const acao = parseInt(prompter()("Digite 1 para banir ou 2 para kickar: "));
-            if (acao != 1 | 2){
-                console.log("Amigão, digitou errado aí")
-            }   
-            adminAction(isAdmin, acao);
-            menu();
-        }
-        else {
-            console.log(`Olá, ${user_to_be.name}! Você não possui permissões de admin.`);
-            menu();
-        }
-    }
-    if (decisao_1 === 4){
-        console.log("Saindo...")
-        return
     }
 }
-
-menu()
+menu();
--- src/admin_to_user_system.ts
+++ src/admin_to_user_system.ts
@@ -10,7 +10,6 @@ function createUser(name: string): User {
     return { name, createdAT: new Date() }
 }
 
-
 interface Admin extends User {
     ban(user: User): void
     kick(user: User): void
@@ -44,90 +43,121 @@ function adminAction(admin: Admin, desicao: number) {
 
     }
 }
+
+function show_menu_actions() {
+    console.log(`
+Olá usuário!
+O que você deseja realizar em nosso sistema?
+-------------------
+1 - Criar usuário
+2 - Promover usuário
+3 - Selecionar usuário
+4 - Sair
+`);
+}
+
 function menu() {
-    console.log("\nOlá usuário!\nO que você deseja realizar em nosso sistema?\n-------------------"
-        , "\n1 - Criar usuário\n2 - Promover usuário\n3 - Selecionar usuário\n4 - Sair")
-    const guia = prompter()("O que deseja fazer?\n: ")
+    show_menu_actions()
+    console.log("O que deseja fazer?")
+    const guia = prompter()(": ")
     const decisao_1: number = parseInt(guia)
+    switch (decisao_1) {
+        case (1):
 
-if (decisao_1 === 1) {
+            const name_to_be_created = prompter()("Nome do usuário a ser criado: ")
+            const newUser = createUser(name_to_be_created);
 
-    const name_to_be_created = prompter()("Qual o nome do usuário a ser criado?")
-    const newUser = createUser(name_to_be_created);
+            const alreadyExists = users.some(user => user.name === name_to_be_created);
+            if (alreadyExists) {
+                console.log(`Usuário ${name_to_be_created} já está registrado!`);
+                menu()
+            } else {
+                users.push(newUser);
+                console.log(`Usuário ${newUser.name} criado com sucesso!`);
+                menu();
+            }
 
-    const alreadyExists = users.some(user => user.name === name_to_be_created);
-    if (alreadyExists) {
-        console.log(`Usuário ${name_to_be_created} já está registrado!`);
-        menu()
-    } else {
-        users.push(newUser);
-        console.log(`Usuário ${newUser.name} criado com sucesso!`);
-        menu();
+            break
+        case (2):
+
+            // var users_concatenada: string = ""
+
+            // for (let i = 0; i < users.length; i++) {
+            //     users_concatenada += `|\t${users[i].name}\t`
+            // }
+
+            // console.log(`Lista de usuários registrados = ${users_concatenada}`)
+            
+            console.log("Usuários registrados:");
+            users.forEach(user => {
+            console.log(`- ${user.name}`);
+            });
+
+            console.log("Administradores registrados:");
+            admins.forEach(admin => {
+            console.log(`- ${admin.name}`);
+            });
+
+            const choiced_user_name: string = prompter()("Digite qual usuário você deseja pegar: ");
+            const foundUser = users.find(user => user.name === choiced_user_name);
+
+            if (foundUser) {
+                const promoted_adminn = promoteUser(foundUser);
+                admins.push(promoted_adminn)
+                console.log(`Usuário ${foundUser.name} promovido a admin!`);
+                menu()
+            } else {
+                console.log(`Usuário ${choiced_user_name} não encontrado.`);
+                menu()
+            }
+            break
+        case (3):
+
+            // let admins_concatenada: string = ""
+            // for (let i = 0; i < admins.length; i++) {
+            //     admins_concatenada += `|\t${admins[i].name}\t`
+            // }
+            console.log("Usuários registrados:");
+            users.forEach(user => {
+                console.log(`- ${user.name}`);
+            });
+            
+            console.log("Administradores registrados:");
+            admins.forEach(admin => {
+                console.log(`- ${admin.name}`);
+            });
+            
+            const found_user_name = prompter()("Qual usuário você deseja ser? ");
+            const user_to_be = users.find(user => user.name === found_user_name);
+
+            if (!user_to_be) {
+                console.log(`Usuário ${found_user_name} não encontrado.`);
+                menu();
+                return;
+            }
+
+            const isAdmin = admins.find(admin => admin.name === user_to_be.name);
+
+            if (isAdmin) {
+
+                console.log(`Olá, Admin ${isAdmin.name}!`);
+                const acao = parseInt(prompter()("Digite 1 para banir ou 2 para kickar: "));
+                adminAction(isAdmin, acao);
+                menu();
+
+            } else {
+
+                console.log(`Olá, ${user_to_be.name}! Você não possui permissões de admin.`);
+                menu();
+
+            }
+
+            break
+        case (4):
+
+            console.log("Saindo...")
+            return
+
     }
 }
-
-
-    let users_concatenada: string = ""
-
-    if (decisao_1 === 2) {
-
-        for (let i = 1; i < users.length; i++) {
-            users_concatenada += `|\t${users[i].name}`
-        }
-        console.log(`Lista de usuários registrados = ${users_concatenada}`)
-         
-        const choiced_user_name: string = prompter()("Digite qual usuário você deseja pegar: ");
-        const foundUser = users.find(user => user.name === choiced_user_name);
-        
-        if (foundUser) {
-            const promoted_adminn = promoteUser(foundUser);
-            admins.push(promoted_adminn)
-            console.log(`Usuário ${foundUser.name} promovido a admin!`);
-            menu()
-        } else {
-            console.log(`Usuário ${choiced_user_name} não encontrado.`);
-            menu()
-        }
-        
-    }
-    if (decisao_1 === 3) {
-        const found_user_name = prompter()("Qual usuário você deseja ser? ");
-        const user_to_be = users.find(user => user.name === found_user_name);
-    
-        if (!user_to_be) {
-            console.log(`Usuário ${found_user_name} não encontrado.`);
-            menu();
-            return;
-        }
-    
-        const isAdmin = admins.find(admin => admin.name === user_to_be.name);
-    
-        if (isAdmin) {
-            console.log(`Olá, Admin ${isAdmin.name}!`);
-            const acao = parseInt(prompter()("Digite 1 para banir ou 2 para kickar: "));
-            adminAction(isAdmin, acao);
-            menu();
-        } else {
-            console.log(`Olá, ${user_to_be.name}! Você não possui permissões de admin.`);
-            menu();
-        }
-    }
-    
-
-}
-// function adminConsole() {
-//     console.log(`Olá usuário ${}`)
-// }
-
-// // const adminKenzo = promoteUser(finalUser)
-
-// if (finalUser.name === "Kenzo") {
-//     const inp = prompter()("Enter your decision: ");
-//     const decision: number = parseInt(inp);
-//     if (decision === 1) {
-//         adminAction(adminKenzo, decision)
-//     }
-// }
-// else {
-//     console.log(`Como você ${finalUser.name} não possui permissões de Admin,\n Não foi possível Banir/Kickar o usuário!`)
-// }
+menu()
